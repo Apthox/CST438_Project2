c9bc855eb40a6ff18011e56961de43be67f85fb5
Apthox <kevin@apthos.net>
2020-02-27 16:55:22 -0800

    added database stuff

--- app.js
+++ app.js
@@ -24,18 +24,27 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
+
+if (process.argv.length > 2) {
+    let arguement = process.argv[2];
+    if (arguement == "dev") {
+        console.log("Connecting to local development database");
+    } else {
+        console.log("connecting to remote database");
+    }
+}
 
 module.exports = app;
